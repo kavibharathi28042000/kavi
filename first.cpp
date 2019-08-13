a69962bc4e7aaf24c1714b8ab54270d838c405be
kavibharathi28042000 <54067705+kavibharathi28042000@users.noreply.github.com>
2019-08-13 18:33:35 +0530

    Create first.cpp

---
+++ first.cpp
@@ -0,0 +1,22 @@
+#include<iostream.h>
+using namespace std;
+int main()
+{
+int num;
+cout<<"enter a number";
+cin>>num;
+if(num>0)
+{ 
+cout<<"the number is positive";
+}
+ else if(num<0)
+{ 
+cout<<"the number is negative";
+}
+else
+{
+cout<<"the number is zero";
+}
+return 0;
+
+}
