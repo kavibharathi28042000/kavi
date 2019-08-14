003b15c3f929c008fa43bdd66e5659d6ee57a034
kavibharathi28042000 <54067705+kavibharathi28042000@users.noreply.github.com>
2019-08-14 15:51:38 +0530

    Create vowel.cpp

---
+++ vowel.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+ char c;
+ cin >>c;
+ if((c>='a' && c<='z') || (c>='A' && c<='Z'))
+ {
+ if  (c=='a'||c=='e'||c=='i'||c=='o'||c=='u')
+ {
+ cout<<"Vowel";
+ }
+ else
+ {
+ cout<<"Consonant";
+ }
+ }
+ else
+    {
+        cout<<"invalid";
+    }
+ return 0;
+}
